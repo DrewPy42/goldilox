6f565f992f5aee610dc2f4f060ae6bcfb229581d
DrewPy42 <mrdrew72@gmail.com>
2023-05-08 11:31:14 -0500

    here we go

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <mainMenu />
+  </header>
+
+  <RouterView />
+  <footer>
+    <a href="https://depositphotos.com/vector-images/objects.html">Green and Yellow Printed Circuit Board - depositphotos.com</a>
+  </footer>
+</template>
+
+
+<script>
+  import { RouterView } from 'vue-router';
+  import mainMenu from './components/menus/mainMenu.vue';
+  export default {
+  components: {
+      mainMenu,
+    },
+    setup() {
+
+    }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/menus/mainMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <img class="navbar-brand fit-image" src="../../../public/logo-lg.png" alt="DataMAP Logo">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            to="/"
+          >
+          Home
+         </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+ import { RouterLink } from 'vue-router'
+
+</script>
+
+<style scoped lang="scss">
+  @import "../../styles/menus.scss";
+</style>
